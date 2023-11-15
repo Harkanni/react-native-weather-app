30a2797ded064f7b568da6b1f7555cfa68b3468b
harkanni <harkanzyempire001@gmail.com>
2023-11-15 11:10:48 +0100

    openweather api call

--- App.js
+++ App.js
@@ -2,19 +2,42 @@ import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import * as Location from 'expo-location'
 
 import Tabs from './src/components/Tabs'
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  if(loading) {
-   return (
+  const [loading, setLoading] = useState(false)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      // console.log(status)
+      if (status !== 'granted') {
+        setError('Permission to access location was denied')
+        return
+      }
+
+      console.log('Permission granted')
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if(location) {
+   console.log(location)
+  }
+
+  if (loading) {
+    return (
       <View style={styles.container}>
-         <ActivityIndicator />
+        <ActivityIndicator />
       </View>
-   )
+    )
   }
   return (
     <NavigationContainer>
@@ -24,10 +47,10 @@ const App = () => {
 }
 
 const styles = StyleSheet.create({
-   container: {
-      justifyContent: 'center',
-      flex: 1,
-   },
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
 })
 
 export default App
